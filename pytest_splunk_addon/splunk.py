cace4abda534229002d5655f650dec66fde67878
Nandini Vij <nvij@splunk.com>
2020-05-05 18:06:52 -0700

    checked out splunk .py to develop

--- pytest_splunk_addon/splunk.py
+++ pytest_splunk_addon/splunk.py
@@ -21,69 +21,6 @@ RESPONSIVE_SPLUNK_TIMEOUT = 300  # seconds
 
 LOGGER = logging.getLogger("pytest-splunk-addon")
 
-@pytest.fixture(scope="session")
-def fetch_cimreq_location(request):
-    package_path = request.config.getoption("splunk_app")
-    folder_path = os.path.join(
-      str(package_path), "cim_reqs"
-    )
-    file_list = []
-    for file in os.listdir(folder_path):
-        filename = os.path.join(folder_path, file)
-        if filename.endswith(".log"):
-            file_list.append(filename)
-    LOGGER.info("file_list -----: %s", file_list)
-    return file_list               
-
-def fetch_model_names_xml(file_name):
-    root = None
-    tree = ET.parse(file_name)
-    root = tree.getroot()
-    model_list = []
-    for model in root.iter('model'):
-        model_list.append(str(model.text))
-    return model_list
-
-def get_event_requirement_file(file_name):
-    root = None
-    tree = ET.parse(file_name)
-    root = tree.getroot()
-    for raw in root.iter('raw'):
-        event = raw.text
-    return event
-
-testdata = [
-    "Network_Sessions",
-    "Network_Traffic",
-]
-@pytest.fixture(scope="module", params=testdata)
-def model(request):
-    model = request.param
-    yield model
-    print("finalizing ")
-
-@pytest.fixture(scope="session")
-def get_model_name(pytestconfig,request):
-    requirements_file_path = request.config.getoption("splunk_app")
-    file_path = os.path.join(
-      str(requirements_file_path), "cim_reqs/cisco_req.log"
-    )
-    req_file = open(file_path, "r")
-    model_names = fetch_model_names_xml(req_file)
-    LOGGER.info("model_name: %s", model_names)
-    return model_names
-
-@pytest.fixture(scope="session")
-def get_event(pytestconfig,request):
-    requirements_file_path = request.config.getoption("splunk_app")
-    file_path = os.path.join(
-      str(requirements_file_path), "cim_reqs/cisco_req.log"
-    )
-    req_file = open(file_path, "r")
-    event = get_event_requirement_file(req_file)
-    LOGGER.info("event:%s", event)
-    return event
-
 
 def pytest_addoption(parser):
     """Add options for interaction with Splunk this allows the tool to work in two modes
