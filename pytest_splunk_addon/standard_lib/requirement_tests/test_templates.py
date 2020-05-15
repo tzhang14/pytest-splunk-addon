f53913dde753fba057571d4192e72ef27a6a4a6b
Nandini Vij <nvij@splunk.com>
2020-05-15 09:23:14 -0700

    comment removed and readability

--- pytest_splunk_addon/standard_lib/requirement_tests/test_templates.py
+++ pytest_splunk_addon/standard_lib/requirement_tests/test_templates.py
@@ -14,31 +14,29 @@ class ReqsTestTemplates(object):
 
     @pytest.mark.splunk_addon_searchtime
     def test_cim_params(self,splunk_searchtime_requirement_param,splunk_search_util):
-
-        #to do check if xml file has bad format
         model = splunk_searchtime_requirement_param["model"]
         escaped_event = splunk_searchtime_requirement_param["escaped_event"]
         unescaped_event = splunk_searchtime_requirement_param["unescaped_event"]
         filename = splunk_searchtime_requirement_param["filename"]
         sourcetype = splunk_searchtime_requirement_param["sourcetype"]
-
-        if ((model == None) and (escaped_event != None)):
-            logging.info("No model present in file")
-            assert False
-        if ((model == None) or (escaped_event == None)):
+        result = False
+        if model is None and escaped_event is None:
             logging.info("Issue parsing log file")
             logging.info("Filename {}".format(filename))
-            assert False
-        if (sourcetype == None):
+            assert result
+        if model is None and escaped_event is not None:
+            logging.info("No model present in file")
+            assert result
+        if sourcetype is None:
             logging.info("Issue finding sourcetype")
-            assert False
+            assert result
         
-        #adding to ingest data
+        # adding to ingest data
         search = f"|makeresults |eval _raw = \"{unescaped_event}\" |collect index=main sourcetype={sourcetype} source=pytest"
-        result1 = splunk_search_util.checkQueryCountIsGreaterThanZero(
+        ingest_flag = splunk_search_util.checkQueryCountIsGreaterThanZero(
             search, interval=INTERVAL, retries=RETRIES
         )
-        while not result1:
+        while not ingest_flag:
             time.sleep(1)
         #to check the datamodel
         search =f"| datamodel {model}  search | search source=pytest {escaped_event}"
@@ -46,14 +44,3 @@ class ReqsTestTemplates(object):
             search, interval=INTERVAL, retries=RETRIES
         )
         assert result
-    # @pytest.mark.splunk_addon_searchtime
-    # def test_ingest(self,splunk_search_util,splunk_searchtime_requirement_file):
-    #     search = "|makeresults |eval _raw = \"May 10 07:15:20 dummy.dvc %ASA-2-106001:Inbound TCP connection denied from 10.0.0.1/1111 to 10.0.0.2/2222 flags dummy_tcp_flags on interface dummy_interface_name\" |collect index=main sourcetype=cisco:asa source=test1234"
-    #     result = splunk_search_util.checkQueryCountIsGreaterThanZero(
-    #         search, interval=INTERVAL, retries=RETRIES
-    #     )
-    #     search = "| datamodel Network_Traffic search | search May 10 07:15:20 dummy.dvc %ASA-2-106001: Inbound TCP connection denied from 10.0.0.1/1111 to 10.0.0.2/2222 flags dummy_tcp_flags on interface dummy_interface_name"
-    #     result = splunk_search_util.checkQueryCountIsGreaterThanZero(
-    #         search, interval=INTERVAL, retries=RETRIES
-    #     )
-    #     assert result
